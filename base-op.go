40e631bb6aecf6ebbeeb9327614a0450c28eb660
plastikfan <plastikfan@outlook.com>
2024-10-23 08:00:28 +0100

    ref(nef): deduplicate aggregate operations (#18)

---
+++ base-op.go
@@ -0,0 +1,18 @@
+package nef
+
+type baseOp[F ExistsInFS] struct {
+	fS   F
+	root string
+}
+
+func (m *baseOp[F]) peek(name string) (exists, isDir bool) {
+	if m.fS.DirectoryExists(name) {
+		return true, true
+	}
+
+	if m.fS.FileExists(name) {
+		return true, false
+	}
+
+	return false, false
+}
